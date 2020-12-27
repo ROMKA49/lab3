c87ad2fb0ecd4bfde86c2bc9aec436beb5600be5
ROMKA49 <72030307+ROMKA49@users.noreply.github.com>
2020-12-27 17:58:56 +0300

    Add files via upload

---
+++ blakdjek.py
@@ -0,0 +1,24 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list = [1,6,7,8,9,10,2,3,4,11] * 4
+import random
+random.shuffle(list)
+print('Поиграем в 21?')
+count = 0
+while True:
+    choice = input('добавитькарту? y/n\n')
+    if choice == 'y':
+        current = list.pop()
+        print('Вам попалась карта достоинством %d' %current)
+        count += current
+        if count > 21:
+            print('Извините, но вы проиграли')
+            break
+        elif count == 21:
+            print('Поздравляю, вы набрали 21!')
+            break
+        else:
+            print('У вас %d очков.' %count)
+    elif choice == 'n':
+        print('У вас %d очков и вы закончили игру.' %count)
+        break
