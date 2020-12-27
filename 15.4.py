e16da1de6e8270757fc84e5f7df8dea78d86d94a
ROMKA49 <72030307+ROMKA49@users.noreply.github.com>
2020-12-27 17:36:02 +0300

    Add files via upload

---
+++ 15.4.py
@@ -0,0 +1,19 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> класс  Point :
+    х = 0
+    у = 0
+    def  setx ( self , x ):
+        я . х = х
+    def  sety ( self , y ):
+        я . у = у
+    def  getx ( сам ):
+        вернуть  себя . Икс
+    def  gety ( сам ):
+        вернуть  себя . у
+
+point = Point ()
+точка . setx ( x = int ( input ( "Введите x" )))
+точка . sety ( y = int ( input ( "Введите y" )))
+печать ( точка . getx ())
+печать ( точка . gety ())
